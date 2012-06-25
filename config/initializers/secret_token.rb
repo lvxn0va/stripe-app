398de8fac2a09c5c2f049a9874d12db56b5a6ae9
lvxn0va <lvx.n0va@gmail.com>
2012-06-25 15:32:48 -0700

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StripeApp::Application.config.secret_token = '83c27f45e43c4a653a78eff5fcecc57bafd2888977799e2c54fdb63586e58f4bbade0221f61812fe7567586309c212075d9b2614687d1272dd972a7e027f892b'
